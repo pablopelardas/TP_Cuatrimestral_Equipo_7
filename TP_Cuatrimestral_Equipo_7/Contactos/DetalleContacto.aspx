e879d7813255621fdbae8fd88e1d960c3d4ede9d
Pablo Pelardas <pablo.pelardas@gmail.com>
2024-05-27 15:00:13 -0300

    Aplicacion>> vista contacto sin data todavia

---
+++ TP_Cuatrimestral_Equipo_7/Contactos/DetalleContacto.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LayoutNegocio.Master" AutoEventWireup="true" CodeBehind="DetalleContacto.aspx.cs" Inherits="TP_Cuatrimestral_Equipo_7.Contactos.DetalleContacto" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%-- DIV Detalles de contacto  --%>
+    <%-- DIV Informacion personal  --%>
+    <%-- DIV Fechas importantes  --%>
+    <%-- DIV Ordenes  --%>
+
+</asp:Content>
---
+++ TP_Cuatrimestral_Equipo_7/Contactos/DetalleContacto.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP_Cuatrimestral_Equipo_7.Contactos
+{
+    public partial class DetalleContacto : System.Web.UI.Page
+    {
+        private Dominio.Modelos.ContactoModelo contacto;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string id = Request.QueryString["id"];
+                Negocio.Servicios.ContactoServicio negocio = new Negocio.Servicios.ContactoServicio();
+                if (id == null) Response.Redirect("/Dashboard.aspx", false);
+                try
+                {
+                    int idInt = Convert.ToInt32(Request.QueryString["id"]);
+                    if (idInt > 0)
+                    {
+                        contacto = negocio.ObtenerPorId(idInt);
+                        if (contacto != null)
+                        {
+                            // CARGAR LOS DATOS DEL CONTACTO EN LOS CONTROLES
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Redirect("/Dashboard.aspx", false);
+                }
+
+            }
+
+        }
+    }
+}
