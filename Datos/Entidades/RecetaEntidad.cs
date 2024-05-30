fecca49dd149ab0b6f4503939696f9abe021cd8e
PabloMairal <pablomairal@gmail.com>
2024-05-30 11:38:45 -0300

    Agrego entidades

---
+++ Datos/Entidades/RecetaEntidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    public class RecetaEntidad
+    {
+        public int id_receta { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int id_categoria { get; set; }
+        public decimal precio_personalizado { get; set; }
+        public RecetaEntidad() { }
+    }
+}
--- Dominio/Modelos/IngredienteModelo.cs
+++ Dominio/Modelos/IngredienteModelo.cs
@@ -10,7 +10,7 @@ namespace Dominio.Modelos
     {
         public int IdIngrediente {  get; set; }
         public string Nombre { get; set; }
-        public float Cantidad { get; set; }
+        public double Cantidad { get; set; }
         public UnidadMedidaModelo Unidad { get; set; }
         public decimal Costo { get; set; }
         public string Proveedor { get; set; }
