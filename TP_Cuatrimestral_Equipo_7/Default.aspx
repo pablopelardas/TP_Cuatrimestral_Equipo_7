7f001a145124d29855e1407865e0e8220c9ac99a
Emanuel <emanuelmonardo@gmail.com>
2024-06-13 01:28:29 -0300

    Creacion Master para el inicio (vacio)

--- TP_Cuatrimestral_Equipo_7/Default.aspx
+++ TP_Cuatrimestral_Equipo_7/Default.aspx
@@ -1,4 +1,14 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/LayoutTailwind.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TP_Cuatrimestral_Equipo_7.Default" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Public.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TP_Cuatrimestral_Equipo_7.Default" %>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1 class="text-3xl underline">Dashboard</h1>
+    <header>
+       
+    </header>
+    <columns>
+        <asp:Button ID="btnVerProductos" runat="server" Text="Ver productos" />
+    </columns>
+    <columns>
+        <asp:Button ID="btnGenerarPedido" runat="server" Text="Generar pedido" />
+    </columns>
+
 </asp:Content>
--- TP_Cuatrimestral_Equipo_7/Public.Master.designer.cs
+++ TP_Cuatrimestral_Equipo_7/Public.Master.designer.cs
@@ -11,25 +11,34 @@ namespace TP_Cuatrimestral_Equipo_7
 {
 
 
-    public partial class Default
+    public partial class Public
     {
 
         /// <summary>
-        /// Control btnVerProductos.
+        /// Control head.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnVerProductos;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
 
         /// <summary>
-        /// Control btnGenerarPedido.
+        /// Control form1.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnGenerarPedido;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
